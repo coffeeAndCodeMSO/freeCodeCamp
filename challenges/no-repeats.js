9da0f6ac1ad66b47edf3d86b56940f73f75778e7
ed42311 <ed42311@gmail.com>
2017-07-31 22:06:43 -0600

    no-repeats, es6 and tests

---
+++ challenges/no-repeats.js
@@ -0,0 +1,31 @@
+function permAlone(str) {
+        var regex = /(.)\1+/g;
+	      var arrayStr = str.split(''); 
+      	function perm(array) {
+      	        var res = [];
+
+      		for(var i=0; i<array.length; i++){
+      			var restA = array.slice(0,i).concat(array.slice(i+1));
+      			var rest  = perm(restA);
+      			if(rest.length === 0){
+      				res.push([array[i]]);
+      			} else {
+      				for(var j=0; j<rest.length; j++){
+      					res.push([array[i]].concat(rest[j]))
+      				}
+      			}
+      		}
+      		return res;
+      	}
+	     var done = perm(arrayStr);
+	     var stringsArray = done.map(function(value) { return value.join('') });
+        var filtered = stringsArray.filter(function(string) {
+            return !string.match(regex);
+        });
+
+        return filtered.length;
+}
+
+permAlone("aab");
+
+module.exports.repeats = permAlone;
---
+++ test/no_repeats_please_test.js
@@ -0,0 +1,80 @@
+var testFile = require('../challenges/no-repeats.js');
+var mocha = require('mocha');
+var expect = require('expect.js');
+
+
+var runTest = function(spec) {
+    it(spec.description, function() {
+        expect(spec.input).to.equal(spec.result);
+    });
+};
+
+describe('repeats test suite', function() {
+    runTest({
+        description: 'repeats("aab") should return a number.',
+        input: typeof testFile.repeats('aab'),
+        result: 'number'
+    });
+
+    runTest({
+        description: 'repeats("aab") should return 2.',
+        input: testFile.repeats('aab'),
+        result: 2
+    });
+
+    runTest({
+        description: 'repeats("aaa") should return 0.',
+        input: testFile.repeats('aaa'),
+        result: 0
+    });
+
+    //permAlone("aabb") should return 8.
+    runTest({
+        description: 'repeats("aabb") should return 8.',
+        input: testFile.repeats('aabb'),
+        result: 8
+    });
+
+    //permAlone("abcdefa") should return 3600.
+    runTest({
+        description: 'repeats("abcdefa") should return 3600.',
+        input: testFile.repeats('abcdefa'),
+        result: 3600
+    });
+
+    //permAlone("abfdefa") should return 2640.
+    runTest({
+        description: 'repeats("abfdefa") should return 2640.',
+        input: testFile.repeats('abfdefa'),
+        result: 2640
+    });
+
+    //permAlone("zzzzzzzz") should return 0.
+    runTest({
+        description: 'repeats("zzzzzzzz") should return 0.',
+        input: testFile.repeats('zzzzzzzz'),
+        result: 0
+    });
+
+    //permAlone("a") should return 1.
+    runTest({
+        description: 'repeats("a") should return 1.',
+        input: testFile.repeats('a'),
+        result: 1
+    });
+
+    //permAlone("aaab") should return 0.
+    runTest({
+        description: 'repeats("aaab") should return 0.',
+        input: testFile.repeats('aaab'),
+        result: 0
+    });
+
+    //permAlone("aaabb") should return 12.
+    runTest({
+        description: 'repeats("aaabb") should return 12.',
+        input: testFile.repeats('aaabb'),
+        result: 12
+    });
+
+})
