58e39d7d3d5a0e58a6e90fc9f3fa6d29586e22d1
Kirk <kirk.douglas.richardson@gmail.com>
2017-08-13 20:02:07 -0600

    Removed Challenge Solution

--- challenges/validate_telephone_numbers.js
+++ challenges/validate_telephone_numbers.js
@@ -10,8 +10,7 @@ See Instructions below
 
 function telephoneCheck(str) {
   // Good luck!
-  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
-  return regex.test(str);
+  return true;
 }
 
 module.exports.telephoneCheck = telephoneCheck;
